743040e9b2fd4413ff5d7f8a5f5564ab29348313
saeedahmed786 <saeedchachar987654@gmail.com>
2024-10-16 20:36:14 +0500

    Updated

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -5,8 +5,8 @@ import HeaderSlider from "./HeaderSlider";
 const Hero = () => {
     return (
         <>
-            <div className="bg-[#F3EFE0] grid grid-cols-1 md:grid-cols-2 gap-10 items-center text-black px-6 py-10 md:py-20" id="home">
-                <div data-aos="fade-right" data-aos-duration="2000" className="order-1 md:order-0">
+            <div className="bg-[#F3EFE0] grid grid-cols-1 md:grid-cols-2 gap-10 items-center text-black md:px-6 md:py-20" id="home">
+                <div data-aos="fade-right" data-aos-duration="2000" className="order-1 md:order-0 px-6 py-6 md:px-0 md:py-0">
                     <h1 className="mb-5">Bienvenue chez BH FERMETURE</h1>
                     <p className="text-lg">
                         Services professionnels de serrurerie, de menuiserie métallique PVC et aluminium. Offrant un savoir-faire de haute qualité pour tous vos besoins en matière de sécurité et de ferronnerie. Contactez-nous pour un devis !
